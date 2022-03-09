701aaf0e9e5d83947a41c0c158a51fbbacf457a7
Florian Reitz <flo@reitz.dev>
2022-03-09 18:39:46 +0000

    added perfect-numbers

---
+++ javascript/perfect-numbers/perfect-numbers.js
@@ -0,0 +1,38 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Programación de Aplicaciones Interactivas 2021-2022
+ *
+ * @author Florian Reitz
+ * @since Marzo 3 2022
+ * @desc Perfect Numbers
+ * @see {@link https://exercism.org/tracks/javascript/exercises/perfect-numbers}
+ *
+ */
+
+'use strict';
+
+export const classify = (number) => {
+  if (number < 1 || parseInt(number) !== number) {
+    throw new Error('Classification is only possible for natural numbers.');
+  }
+
+  let sumOfFactors = getSumOfFactors(getFactors(number));
+
+  return sumOfFactors < number ? 'deficient' : 
+    sumOfFactors > number ? 'abundant' : 'perfect';
+};
+
+const getSumOfFactors = (factors) => {
+  if(factors.length === 0) return 0;
+  return factors.reduce((number1, number2) => number1 + number2);
+}
+
+const getFactors = (number) => {
+  let factors = [];
+  for (let i = 1; i < number; i++){
+    if (number % i == 0) factors.push(i);
+  }
+  return factors;
+}
